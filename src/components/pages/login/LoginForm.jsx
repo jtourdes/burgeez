f3c8ab2ea68393cbe91beb4e428c301740b1156f
Ryzoma <j.tourdes@gmail.com>
2023-02-23 16:15:24 +0100

    🎨 F01 LoginPage > Refactoring

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,24 +1,34 @@
-import { logDOM } from '@testing-library/react';
 import React, { useState, useEffect } from 'react';
 
-
 const LoginForm = () => {
+    //State
     const [name, setName] = useState("");
 
-    function onChange (event) {
+    //Comportement
+    function handleChange (event) {
         setName(event.target.value)
     }
 
-    function onClick (){
-        alert("Bonjour "+ name)
+    function handleSubmit (event){
+        event.preventDefault()
+        alert(`Bonjour ${name}`)
     }
 
+    //Render
     return (  
         <div>
+            <form action="submit" onSubmit={handleSubmit}>
+            <h1>Bienvenue chez nous !</h1>
+            <br />
             <h2>Connectez-vous :</h2>
-            <form action="">
-                <input type="text" placeholder="Entrez votre prénom ..." required={true} onChange={onChange}/>
-                <button onClick={onClick} type="submit">Accédez à votre espace</button>
+                <input 
+                    value={name} 
+                    type="text" 
+                    placeholder="Entrez votre prénom ..." 
+                    required={true} 
+                    onChange={handleChange}
+                />
+                <button type="submit">Accédez à votre espace</button>
             </form>
         </div>
     );
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -4,7 +4,6 @@ import LoginForm from './LoginForm';
 const LoginPage = () => {
     return (  
         <div>
-            <h1>Bienvenue chez nous !</h1>
             <LoginForm />
         </div>
     );
