2c07e4f6b70a46723c067f56bb31532158194918
Ryzoma <j.tourdes@gmail.com>
2023-03-08 12:50:27 +0100

    ✨ F02 APP > Install rooting system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import {Routes, Route} from "react-router-dom"
 import LoginPage from "./components/pages/login/LoginPage"
+import OrderPage from './components/pages/order/OrderPage';
+import ErrorPage from './components/pages/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <Routes> // define acessible routes inside the component
+      <Route path="/" element={<LoginPage />}/> //precise for each route a URL and a component
+      <Route path="/order" element={<OrderPage />}/>
+      <Route path="*" element={<ErrorPage />}/>
+    </Routes>
+      
   );
 }
 
