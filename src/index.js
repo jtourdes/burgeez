70ad14e432fb8ec72c909f5fde17d515d820154c
Ryzoma <j.tourdes@gmail.com>
2023-03-08 12:38:35 +0100

    ✨ 🔥 F02 Index.js > switch to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom  "
 import './index.css';
 
 import App from './App';
@@ -15,28 +12,12 @@ import OrderPage from './components/pages/order/OrderPage';
 import reportWebVitals from './reportWebVitals';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/order",
-    element: <OrderPage name={""}/>,
-    errorElement: <ErrorPage />
-  },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
