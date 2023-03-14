9894a86d8f6f7d8ed26b9cdc58ca65f9c4fa3121
Ryzoma <j.tourdes@gmail.com>
2023-03-14 22:30:24 +0100

    ♻️ F03 > LoginForm : hydration of some components

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import styled from 'styled-components';
-import { BsPersonCircle } from "react-icons/bs";
 import {BsFillArrowRightCircleFill} from "react-icons/bs"
+import { BsPersonCircle } from "react-icons/bs";
+import Input from './Input';
+import PrimaryButton from './PrimaryButton';
 // import theme from "../../../theme/index.js"
-
 
 
 const LoginForm = () => {
     //State
-    const [username, setUserName] = useState("");
     const navigate = useNavigate();
+    const [username, setUserName] = useState("");
 
     //Comportement
     function handleChange(event) {
@@ -28,24 +29,20 @@ const LoginForm = () => {
             <h1>
                 Bienvenue chez nous !
             </h1>
-      
             <div className="breakline" />
             <h2>Connectez-vous :</h2>
             <div className="input-container">
-                <div className="input-with-icon">
-                    <BsPersonCircle className='profile-icon'/>
-                    <input
-                        value={username}
-                        type="text"
-                        placeholder="Entrez votre prénom ..."
-                        required={true}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">
-                    <span>Accédez à mon espace</span>
-                    <BsFillArrowRightCircleFill className="arrow-icon"/>
-                </button>
+                <Input
+                    value={username}
+                    onChange={handleChange}
+                    Icon={<BsPersonCircle className="profile-icon"/>}
+                    placeholder={"Entrez votre prénom ..."}
+                    required={true}
+                />
+                <PrimaryButton 
+                    label={"Accédez à mon espace"}
+                    Icon={ <BsFillArrowRightCircleFill className="arrow-icon"/>}
+                />
             </div>
         </Form>
     );
@@ -68,64 +65,19 @@ const Form = styled.form`
         font-weight: 700;
     }
 
-    input {
-        font-size:1rem;
-        font-weight: bold;
-        border: none;     
-    }
-
     .input-container {
         display: flex;
         flex-direction: column;
         align-items: center;
         gap: 1rem;
     }
-
-    .input-with-icon{
-            height: 4rem;
-            min-width: 20rem;
-            max-width: 30rem;
-            border-radius: 7px;
-            border-style: none;
-            color:white;   
-            background-color: white;
-            display: flex;
-            align-items: center;
-    }
-
-    button {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 4rem;
-        min-width: 20rem;
-        max-width: 30rem;
-        background-color: orange;
-        border-radius: 7px;
-        border-style: none;
-        color: white;
-        font-weight: bold;
-        font-size: 1rem;
-        padding: 0rem 0.5rem;
-    }
-
-    .profile-icon {
-        color: grey;
-        margin: 1rem
-    }
-    .arrow-icon {
-        margin: 0.5rem
-    }
-
+    
     .breakline {
         height: 4px;
         width: 21rem;
         background-color: orange;
         margin: auto;
     }
-
-
-
 `
  
 
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -12,7 +12,6 @@ const LoginPage = () => {
     );
 }
 
-
 const LoginPageStyled = styled.div`
     height: 100vh;
     display: flex;
