47313e28e5ad4cc971fb0cb9fe35277c4c2b1edc
Ryzoma <j.tourdes@gmail.com>
2023-03-17 15:11:05 +0100

    📦 💄 F05 > utils / thme : added math.js / added shadow properties

--- src/theme/index.js
+++ src/theme/index.js
@@ -57,7 +57,11 @@ const borderRadius = {
   extraRound: "15px",
   circle: "50%",
 }
-const shadows = {}
+const shadows = {
+  small: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+  medium: "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
+  large: "rgba(0, 0, 0, 0.35) 0px 5px 15px;"
+}
 
 export const theme = {
   colors,
